0be9fa21804f142d15d62b26d7e8d9823129d623
suttondaniel <dansut13@gmail.com>
2022-08-08 21:09:47 -0700

    streamlining packages

--- fit.py
+++ fit.py
@@ -1,221 +1,125 @@
 import pandas as pd
-from pandas.io.json import json_normalize
-pd.options.display.max_rows = 999
+#pd.options.display.max_rows = 999
 
-import time
-import datetime as dt
-import numpy as np
-import shutil
-import os
+#import time
+#import datetime as dt
+#import numpy as np
 from pathlib import Path
-import requests
-import json
-import csv
+#import requests
+#import json
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-import plotly.express as px
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None
 
 import streamlit as st
-import time
+#import time
 
-pd.options.mode.chained_assignment = None
+#import plotly.express as px
+#import plotly.graph_objs as go
+#import matplotlib.pyplot as plt
+
+def prepare_data():
+    strava_activities_raw = pd.read_csv('strava_activities.csv')
+
+    cols_to_drop = ['Unnamed: 0.1', 'athlete', 'resource_state', 'start_date_local', 'timezone', 
+    'utc_offset', 'trainer', 'commute', 'type', 'manual', 'visibility', 'flagged', 'gear_id', 'start_latlng', 'end_latlng', 'display_hide_heartrate_option',
+    'upload_id_str', 'external_id', 'from_accepted_tag', 'total_photo_count', 'athlete.resource_state', 'map.id', 'map.summary_polyline', 'map.resource_state', 
+    'average_watts', 'kilojoules', 'device_watts', 'photo_count', 'heartrate_opt_out', 'upload_id', 'athlete.id', 'workout_type',
+    'has_heartrate', 'location_city', 'location_state', 'location_country', 'private', 'has_kudoed', 'Unnamed: 0']
+
+    strava_activities_clean = strava_activities_raw.drop(labels=cols_to_drop, axis=1)
+
+    # meters to miles
+    strava_activities_clean['distance'] = strava_activities_clean.distance * .000621
+
+    # search results for my pace (pick a pace, any pace) and see my suffering go down over time
+    # might be a fun metric to plot over time. perhaps my suffering per minute has gone down as ive gotten fitter
+    strava_activities_clean['SPM'] = strava_activities_clean.suffer_score / (strava_activities_clean.moving_time / 60)
+
+    rename = {'start_date': 'start_time'}
+    strava_activities_clean.rename(rename, axis=1, inplace=True)
+
+    # convert start_time to pandas Timestamp object. then we can access seconds, minutes, hours, etc. and can add the moving_time
+    strava_activities_clean['start_time'] = pd.to_datetime(strava_activities_clean['start_time'])
+
+    # have to manually adjust one activity; the run was on 6/12 not 6/13
+    strava_activities_clean.loc[strava_activities_clean.id == 7299973019, 'start_time'] = pd.Timestamp(year=2022, month=6, day=12, hour=2, minute=8, second=36, tz='UTC')
+
+    strava_activities_clean.set_index('start_time', inplace=True)
+
+    return strava_activities_clean
+
 
 def main():
     # render the readme as markdown using st.markdown
     p = Path.home()
     emojis = str(p / 'Documents' / 'Python' / 'fitprogress' / 'data' / 'emoji.json')
 
-    if os.path.exists(activities_csv_downloaded):
-        download_to_raw()
+    activities = prepare_data()
+
+    types_of_activities = activities.sport_type.unique()
+
+    runs = activities[activities['sport_type'] == 'Run']
+    runs_2022 = runs[runs.index.year == 2022]
+
+    st.title('Strava Dashboard')
+    st.subheader('A Strava dashboard by [Dan Sutton](https://github.com/suttondaniel)')
+
+    miles_run = runs_2022.distance.sum()
+    elev_gain = runs_2022.total_elevation_gain.sum()
+    mpd = runs_2022.distance.sum() / runs_2022.index[0].day_of_year
     
-    st.markdown(open(readme).read())
+    col1, col2, col3 = st.columns(3)
 
-    # read in our processed activities.csv
-    data = pd.read_csv(activities_csv_raw)
-    data = prep_df(data)
-    #data = clean_time(data)
-    #data = cols_to_float(data)
+    # you can pass in a percentage or $ amt as a second variable to show growth or shrinkage over time
 
-    run, bike = split_run_and_bike(data)
-    run = clean_run_pace(run)
+    #col1.metric("Miles Ran", miles_run, "-$1.25")
+    col1.metric("Miles Ran (YTD)", round(miles_run, 2))
+    col2.metric("Elevation Gain (YTD)", round(elev_gain, 2))
+    col3.metric("Miles Per Day", round(mpd, 2))
 
-    # create a clean pace column for output purposes
-    run['pace'] = run.avg_pace.apply(lambda x: str(x).split(' ')[2])
-    
-    bike = clean_bike_pace(bike)
+    st.markdown("### Last 5 Activities: ")
+    st.dataframe(activities.head(5), width=3000)
 
-    # moved the activity selecter to the sidebar, will flesh out other activity monitors later
-    types_of_activities = data.activity_type.unique()
     activity_choice = st.sidebar.selectbox('Select an activity', types_of_activities)
            
-    if activity_choice == 'Running':
-        numtimes = run.shape[0]
+    if activity_choice == 'Run':
+        choices = {'W-Mon': '%A, %b %d %Y', 'M': '%B %Y', 'Y': '%Y'}
+        choices_txt = {'By Week': 'W-Mon', 'By Month': 'M', 'By Year': 'Y'}
 
-        em = pd.read_json(emojis, orient='index')
-        em = list(em.index)
-        emojis = np.random.choice(em, 3)
+        choice_selector = st.radio('Select the time frame you want to see your totals for', choices_txt.keys())
+        choice = choices_txt[choice_selector]
 
-        st.markdown(f'# Running :{emojis[0]}::{emojis[1]}::{emojis[2]}:')
-        st.subheader(f'Select a distance to see your top 5 efforts: ')
-        dists = ['2.0 mi.', '5k', '4.0 mi.', '5.0 mi.', '10k', '7.0 mi.']
-        dist = st.selectbox('Select a distance to see your top 5 efforts', dists) 
+        # #strava_activities_clean[strava_activities_clean['sport_type'] == 'Run'].resample('W-Mon', closed='left').distance.sum().tail(15)
+        df_slice = activities[activities['sport_type'] == activity_choice].resample(choice, closed='left').distance.sum().tail(5)
+        #df_slice.index = df_slice.index.strftime('%B %Y')
+        df_slice.index = df_slice.index.strftime(choices[choice])
+        st.dataframe(df_slice)
+
+
+    
+    
+    # TALLY HOW MANY TIMES YOU DID THE ACTIVITY
+    # if activity_choice == activity_choice:
+    #     numtimes = strava_activities_clean[strava_activities_clean.sport_type == activity_choice].shape[0]
+    #     em = pd.read_json(emojis, orient='index')
+    #     em = list(em.index)
+    #     emojis = np.random.choice(em, 3)
+    #     st.markdown(f'### You\'ve {activity_choice} {numtimes} times over the past 3 years :{emojis[0]}::{emojis[1]}::{emojis[2]}:')
+
+        # st.markdown(f'# Running :{emojis[0]}::{emojis[1]}::{emojis[2]}:')
+        # st.subheader(f'Select a distance to see your top 5 efforts: ')
+        # dists = ['2.0 mi.', '5k', '4.0 mi.', '5.0 mi.', '10k', '7.0 mi.']
+        # dist = st.selectbox('Select a distance to see your top 5 efforts', dists) 
+
+        # dist_int = [i for i in range(2,8)]
+        # dist_choice = dist_int[dists.index(dist)]
+
+        # four_milers = run[round(run.distance, 0) == dist_choice]
         
-        dist_int = [i for i in range(2,8)]
-        dist_choice = dist_int[dists.index(dist)]
-
-        four_milers = run[round(run.distance, 0) == dist_choice]
-        
-        four_milers = four_milers[['distance', 'calories', 'pace', 'avg_run_cadence']]
-        four_milers.sort_values(by='pace', ascending=True, inplace=True)
-        st.dataframe(four_milers.head(5))
-        
-        st.subheader(f'You\'ve done this activity {numtimes} times over the last two years.  \
-                         Select the number you\'d like to display: ')
-        
-        # '''
-        # TO-DO: Flesh out the datetime slider.  it works technically, but the slider logic is backwards.  
-                
-        # date_min_val = run.index[0].to_pydatetime()
-        # date_max_val = run.index[-1].to_pydatetime()
-        # date_starter_val = run.index[5].to_pydatetime()
-        # date_selected = st.slider('Select how far back you want to see: ', min_value=date_min_val, max_value=date_max_val)
-        # run_slice = run[:date_selected]
-        # '''
-
-        # another TO-DO: have a select a date for the start and end of the df slice.  
-        num_runs_selected = st.slider('', 1, run.shape[0], 5)
-        run_slice = run.iloc[:num_runs_selected]
-        #st.write(type(run_slice.distance[2]))
-        # preserve the original run index....
-        run_slice_index_orig = run_slice.index
-
-        # ...then set the index to a more appealing date format, for display purposes only...
-        run_slice.index = run_slice.index.strftime('%m/%d/%Y')
-        st.dataframe(run_slice[['distance', 'calories', 'pace', 'avg_run_cadence']])
-
-        #...then go head and set it right back
-        run_slice.index = run_slice_index_orig
-        
-        num_days = (run_slice.index[0] - run_slice.index[-1]).days
-        avg_miles = round(run_slice.distance.mean(), 2)
-        total_miles = run_slice.distance.sum()
-        mi_per_day = total_miles / num_days
-        avg_pace = str(run_slice.avg_pace.mean()).split(' ')[2].split('.')[0]
-
-        st.markdown(f'Number of days in this slice: **{num_days} days** :sunglasses:')
-        st.markdown(f'Total miles ran in this slice: **{total_miles: .2f} miles** :stuck_out_tongue_winking_eye:')
-        st.markdown(f'Average miles per run in this slice: **{avg_miles: .2f} miles**  :sweat_drops: :sweat_drops:')
-        st.markdown(f'Average miles ran per day: **{mi_per_day: .2f} miles**  baby :video_game:')
-        st.markdown(f'Average pace per run on this slice:  **{avg_pace}** :fire::fire::fire:')        
-        
-        # theme choices: ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]
-
-        choices = ['time', 'avg_run_cadence', 'avg_pace']
-        choice = st.selectbox('Select what you would like to see', choices) 
-        
-        fig = px.scatter(data_frame=run_slice, 
-                        y=choice,   # choice 
-                        hover_data=['distance', 'calories'], 
-                        labels=dict(x='Date', time='Time', avg_run_cadence='Average Run Cadence', avg_pace='Average Pace'), 
-                        title='Snapshot',
-                        size='distance',
-                        color='distance',
-                        template='plotly',
-                        height=700, 
-                        width=1200,
-                        )
-        #if choice == 'avg_pace':
-            #fig.update_yaxes(tickformat='%M:%S')
-        #elif choice == 'time':
-            #fig.update_yaxes(tickformat='%H:%M:%S')
-        
-        #fig.update_xaxes(tickformat='%m/%d/%Y'),
-        st.plotly_chart(fig)
-
-        #last_15_weeks = pd.DataFrame(data[data.activity_type == 'Running'].resample('W-MON').distance.sum().tail(15))
-        #st.line_chart(last_15_weeks)
-
-def prep_df(df):
-    df.columns = df.columns.str.lower().str.replace(' ', '_')
-    df.drop(df.columns[17:29], axis=1, inplace=True)
-    df.drop(['favorite'], axis=1, inplace=True)
-    df.replace('--', '0', inplace=True)
-    df.date = pd.to_datetime(df.date)
-    df.set_index('date', inplace=True)
-    return df
-
-def clean_time(df):
-    #df.time = df.time.split('.')[0]
-   # df.time = df.time.apply()
-    df.time.apply(lambda x: (lambda y: y.split(':')).insert(0, '00') if len(x) < 3 else x)
-    time_pieces = df.time.apply(lambda x: x.split(':'))
-    #df.time = pd.to_timedelta(df.time)
-    #time = time.split('.')[0]
-    #time_pieces = time.split(':')
-    if len(time_pieces) < 3:
-        time_pieces.insert(0, '00')
-    time = ':'.join(time_pieces)
-    time = dt.datetime.strptime(time, '%H:%M:%S')
-    df.time = time
-    return df
-
-def cols_to_float(df):
-    df.calories = df.calories.str.replace(',', '').astype(float)
-    df.distance = df.distance.astype(float)
-    df.iloc[:, 5:10] = df.iloc[:, 5:10].replace('--', '0')
-    df.iloc[:, 5:10] = df.iloc[:, 5:10].astype(float)
-    return df
-
-
-
-def clean_run_pace(df):
-    '''
-    Converts our mm:ss string to a datetime object, then back to a string but in HH:MM:SS format since
-    that's what to_timedelta() takes.  
-
-    Time deltas are for durations, while datetimes are for moments in time.      
-    '''
-    df.best_pace = df.best_pace.apply(lambda x: dt.datetime.strptime(x, '%M:%S'))
-    df.best_pace = df.best_pace.apply(lambda x: x.strftime('%H:%M:%S'))
-    df.best_pace = pd.to_timedelta(df.best_pace)
-    df.avg_pace = df.avg_pace.apply(lambda x: dt.datetime.strptime(x, '%M:%S'))
-    df.avg_pace = df.avg_pace.apply(lambda x: x.strftime('%H:%M:%S'))
-    df.avg_pace = pd.to_timedelta(df.avg_pace)
-    return df
-
-def clean_bike_pace(df):
-    df.avg_pace = df.avg_pace.astype(float)
-    return df
-
-def download_to_raw():
-    activities_df_downloaded = pd.read_csv(activities_csv_downloaded)
-    activities_df_raw = pd.read_csv(activities_csv_raw)
-    activities_df_raw = pd.concat([activities_df_downloaded, activities_df_raw]).drop_duplicates(subset='Date')
-    activities_df_raw.to_csv(activities_csv_raw, index=False)
-    os.remove(activities_csv_downloaded)
-
-
-
-
-
-def split_run_and_bike(df):
-    run = df[df.activity_type == 'Running']
-    bike = df[df.activity_type == 'Cycling']
-    return run, bike
-
-p = Path.home()
-cwd = Path.cwd()
-
-readme = str(p / 'Documents' / 'Python' / 'fitprogress' / 'README.md')
-activities_csv_downloaded = str(p / 'Downloads' / 'Activities.csv')
-activities_csv_raw = str(p / 'Documents' / 'Python' / 'fitprogress' / 'data' / '01_raw' / 'Activities.csv')
-activities_csv_processed = str(p / 'Documents' / 'Python' / 'fitprogress' / 'data' / '02_processed' / 'Activities_processed.csv')
-emojis = str(p / 'Documents' / 'Python' / 'fitprogress' / 'data' / 'emoji.json')
+        # four_milers = four_milers[['distance', 'calories', 'pace', 'avg_run_cadence']]
+        # four_milers.sort_values(by='pace', ascending=True, inplace=True)
+        # st.dataframe(four_milers.head(5))
 
 if __name__ == "__main__":
     main()
-
-    
